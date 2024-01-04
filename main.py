373763bd639bad9590f0433379d42fa819206638
Qetor <elian.fehler@tutanota.com>
2024-01-04 18:32:25 +0100

    fix url subpath issues

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 RECIPE_DIR = os.getenv('RECIPE_DIR')
-SUBPATH = os.getenv('SUBPATH')
 
 if not os.path.exists(RECIPE_DIR):
     os.mkdir(RECIPE_DIR)
 
-main = Blueprint('main', __name__, template_folder='templates', static_folder='static', static_url_path='assets')
+app = Flask(__name__, static_url_path='/static')
+app.config['UPLOAD_FOLDER'] = RECIPE_DIR
 
 def get_recipes_list():
     recipes = []
@@ -21,12 +21,12 @@ def get_recipes_list():
 
     return recipes
 
-@main.route('/')
+@app.route('/')
 def root():
     recipes = get_recipes_list()
     return render_template("index.html", recipes=recipes)
 
-@main.route('/search')
+@app.route('/search')
 def search():
     query = request.args['q']
     
@@ -36,11 +36,11 @@ def search():
     
     return render_template("index.html", recipes=recipes, query=query)
 
-@main.route('/bye')
+@app.route('/bye')
 def bye_bye():
     return jsonify({"message": "cya"})
 
-@main.route('/new', methods=["POST", "GET"])
+@app.route('/new', methods=["POST", "GET"])
 def new_recipe():
     if request.method == "GET":
         return render_template("create.html")
@@ -54,14 +54,14 @@ def new_recipe():
     return redirect(url_for("root"), 302)
     
 
-@main.route('/delete/<name>')
+@app.route('/delete/<name>')
 def delete_recipe(name):
     filepath = os.path.join(RECIPE_DIR, name)
     os.remove(filepath)
 
     return redirect(url_for("root"), 302)
 
-@main.route('/recipe/<name>')
+@app.route('/recipe/<name>')
 def get_recipe(name):
     filepath = os.path.join(RECIPE_DIR, name)
     if not os.path.exists(filepath):
@@ -69,15 +69,11 @@ def get_recipe(name):
 
     return render_template("recipe.html", name=name)
 
-@main.route('/files/<name>')
+@app.route('/files/<name>')
 def download_file(name):
     mime = magic.from_file(os.path.join(RECIPE_DIR, name), mime=True)
     return send_from_directory(RECIPE_DIR, name), {'Content-Type': mime}
 
-app = Flask(__name__, static_url_path='/static')
-app.config['UPLOAD_FOLDER'] = RECIPE_DIR
-app.register_blueprint(main, url_prefix=SUBPATH)
  
-if __name__ == '__main__':
-    app.run(port=8080)
+app.run(port=8080)
 
