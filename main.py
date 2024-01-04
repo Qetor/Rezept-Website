b35db567b6bd4e6ca063731dc0f7e2f3eef2182f
Qetor <elian.fehler@tutanota.com>
2024-01-04 17:20:15 +0100

    New design and more features

--- main.py
+++ main.py
@@ -1,73 +1,83 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-import os
-from flask import Flask, jsonify, render_template, send_from_directory, request, url_for, redirect
+import os, magic
+from flask import Flask, jsonify, render_template, send_from_directory, request, url_for, redirect, Blueprint
+from dotenv import load_dotenv
 
-recipe_dir = "/home/elian/recipes/"
-if not os.path.exists(recipe_dir):
-    os.mkdir(recipe_dir)
+load_dotenv()
+RECIPE_DIR = os.getenv('RECIPE_DIR')
+SUBPATH = os.getenv('SUBPATH')
 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = recipe_dir
+if not os.path.exists(RECIPE_DIR):
+    os.mkdir(RECIPE_DIR)
+
+main = Blueprint('main', __name__, template_folder='templates', static_folder='static', static_url_path='assets')
 
 def get_recipes_list():
     recipes = []
     
-    for filename in os.listdir(recipe_dir):
+    for filename in os.listdir(RECIPE_DIR):
         recipes.append(filename)
 
     recipes.sort()
 
     return recipes
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
+@main.route('/')
 def root():
     recipes = get_recipes_list()
     return render_template("index.html", recipes=recipes)
 
-@app.route('/bye')
+@main.route('/search')
+def search():
+    query = request.args['q']
+    
+    lower_query = query.lower()
+
+    recipes = [recipe.lower() for recipe in get_recipes_list() if lower_query in recipe]
+    
+    return render_template("index.html", recipes=recipes, query=query)
+
+@main.route('/bye')
 def bye_bye():
     return jsonify({"message": "cya"})
 
-@app.route('/new', methods=["POST"])
+@main.route('/new', methods=["POST", "GET"])
 def new_recipe():
+    if request.method == "GET":
+        return render_template("create.html")
+    
     file = request.files.get('image')
     if not file:
         return jsonify({"message": "No file provided!"})
     
-    file.save(os.path.join(recipe_dir, request.form['title']))
+    file.save(os.path.join(RECIPE_DIR, request.form['title']))
 
     return redirect(url_for("root"), 302)
     
 
-@app.route('/delete/<name>')
+@main.route('/delete/<name>')
 def delete_recipe(name):
-    filepath = os.path.join(recipe_dir, name)
+    filepath = os.path.join(RECIPE_DIR, name)
     os.remove(filepath)
 
     return redirect(url_for("root"), 302)
 
-@app.route('/recipe/<name>')
+@main.route('/recipe/<name>')
 def get_recipe(name):
-    filepath = os.path.join(recipe_dir, name)
+    filepath = os.path.join(RECIPE_DIR, name)
     if not os.path.exists(filepath):
         return jsonify({"message": "Recipe doesn't exist!"})
 
     return render_template("recipe.html", name=name)
 
-@app.route('/files/<name>')
+@main.route('/files/<name>')
 def download_file(name):
-    return send_from_directory(recipe_dir, name)
+    mime = magic.from_file(os.path.join(RECIPE_DIR, name), mime=True)
+    return send_from_directory(RECIPE_DIR, name), {'Content-Type': mime}
+
+app = Flask(__name__, static_url_path='/static')
+app.config['UPLOAD_FOLDER'] = RECIPE_DIR
+app.register_blueprint(main, url_prefix=SUBPATH)
  
-# main driver function
 if __name__ == '__main__':
-    # run() method of Flask class runs the application 
-    # on the local development server.
     app.run(port=8080)
 
